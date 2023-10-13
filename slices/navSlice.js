b356dc177ad0075bc67cfb1463a2d2f85dbdf7b7
seydiop <mouhaleecr7@gmail.com>
2023-10-13 08:04:13 +0000

    feat: add NavOptions + HomeScreen

--- slices/navSlice.js
+++ slices/navSlice.js
@@ -11,7 +11,24 @@ export const navSlice = creatSlice({
     initialState,
     reducer: {
         setOrigin: (state, action) => {
-            //Last changes***
-        } 
+            state.origin = action.payload;
+        }, 
+        setDestination: (state, action) => {
+            state.destination = action.payload;
+        },
+        setTravelTimeInformation: (state, action) => {
+            state.travelTimeInformation = action.payload;
+        },
     },
-})
+});
+
+export const { setOrigin, setDestination, setTravelTimeInformation} =
+navSlice.actions;
+
+//Selectors
+export const selectOrigin = (state) => state.nav.origin;
+export const selectDestination = (state) => state.nav.destination;
+export const selectTravelTimeInformation = (state) => 
+state.nav.travelTimeInformation;
+
+export default navSlice.reducer;
